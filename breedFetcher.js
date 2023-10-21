605280727e73ef1dc8330308f74edff500bed508
David Carmichael <carmichael.db88@gmail.com>
2023-10-21 23:12:01 +0000

    refactor code, test errors and create better error logic

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,25 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const breed = args[0];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(`There was a request Error: ${error}`);
-    return;
-  }
-  
-  const data = JSON.parse(body);
-  if (JSON.stringify(data) === '[]') {
-    console.log("Breed not Found");
-  } else {
-    console.log(data[0].description);
-  }
 
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response && response.statusCode > 400) {
+      callback(`There was a request Error: ${response.statusCode}`, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (JSON.stringify(data) === '[]') {
+      callback("Breed not Found", null);
+      return;
+    } else {
+      callback(null, data[0].description);
+      return;
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
